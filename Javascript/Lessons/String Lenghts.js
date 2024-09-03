28cdcf58eede80e8dd33e43cc3a33bc1d9cc33f8
CobaltBrew <workelizar@gmail.com>
2024-09-03 13:58:56 +0800

    1

--- Javascript/Lessons/String Lenghts.js
+++ Javascript/Lessons/String Lenghts.js
@@ -1,5 +1,5 @@
 // String Lenghts and Retrieving the Number of Characters
-// Counts the nmber of characters in a string
+// Counts the number of characters in a string
 // https://charactercounter.com/twitter
 // word.lenght
 
@@ -7,3 +7,13 @@
 
 var name = "Angela";
 name.lenght;
+
+// Challenge Output  how many characters have written and how many characters remaining
+// Example " You have written 182 characters, you have -42 characters left"
+
+
+
+var tweet = prompt ("Compose your tweet");
+var tweetCount = tweet.length
+
+alert("You have written " + tweetCount + "characters, you have " + (140 - tweetCount) + " characters remaining.")
