cb9c5ab0a3fa185c35e5a57937666a21e2ec3ac9
CobaltBrew <workelizar@gmail.com>
2024-09-03 13:28:51 +0800

    5

--- Javascript/Lessons/String Lenghts.js
+++ Javascript/Lessons/String Lenghts.js
@@ -1,6 +1,9 @@
 // String Lenghts and Retrieving the Number of Characters
 // Counts the nmber of characters in a string
+// https://charactercounter.com/twitter
 // word.lenght
+
+
 
 var name = "Angela";
 name.lenght;
